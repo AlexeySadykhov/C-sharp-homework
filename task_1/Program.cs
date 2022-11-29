eb94ff2abc65568c487c6bace53ae4c4b986b4d9
alex <alexeysadykhov@yahoo.com>
2022-11-29 23:55:51 +0400

    First task has been solved

---
+++ task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// task 1
+Console.WriteLine("Enter the first number:");
+string strNumA = Console.ReadLine();
+Console.WriteLine("Enter the second number:");
+string strNumB = Console.ReadLine();
+
+void CompareTwoNums(string numA, string numB)
+{
+    if (int.TryParse(strNumA, out var x) & int.TryParse(strNumB, out var y))
+    {
+        if (x > y)
+        {
+            Console.WriteLine($"max = {x}");
+        }
+        else
+        {
+            Console.WriteLine($"max = {y}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Can't convert to integer.");
+    }
+}
+CompareTwoNums(strNumA, strNumB);
