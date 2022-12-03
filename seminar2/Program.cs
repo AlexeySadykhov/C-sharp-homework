f834928f5bacdc77cd2e185cea40598d8636345a
alex <alexeysadykhov@yahoo.com>
2022-12-03 14:33:21 +0400

    Seminar2 folder added. Tasks 1-2 solved.

---
+++ seminar2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Tasks 1 and 2
+int FindDigit(int n, int d)
+{
+    List<int> remain = new List<int>();
+    while (n > 0)
+    {
+        remain.Add(n % 10);
+        n /= 10;
+    }
+    if (remain.Count < d) return -1;
+    else return remain[remain.Count - d];
+}
+
+void Main()
+{
+    Console.WriteLine("Enter the number:");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter the digit you want to find:");
+    int digit = Convert.ToInt32(Console.ReadLine());
+    int result = FindDigit(number, digit);
+    if (result < 0) Console.WriteLine($"Number has no {digit} digit.");
+    else Console.WriteLine(result);
+}
+Main();
+
