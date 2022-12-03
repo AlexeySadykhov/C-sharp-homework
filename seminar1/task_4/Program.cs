4068ed25efcc86977ac7118d23d6753da813b337
alex <alexeysadykhov@yahoo.com>
2022-12-03 13:56:51 +0400

    Folders structure modified

---
+++ seminar1/task_4/Program.cs
@@ -0,0 +1,25 @@
+﻿void evenRange(int x)
+{
+    if (x <= 1)
+    {
+        Console.WriteLine("Error. Number must be higher than 1.");
+    }
+    else
+    {
+        List<int> nums = new List<int>();
+        for (int i = 1; i <= x; i++)
+        {
+            if (i % 2 == 0)
+            {
+                nums.Add(i);
+            }
+        }
+        var result = string.Join(" ", nums);
+        Console.WriteLine(result);
+    }
+}
+
+Console.WriteLine("Enter number:");
+string strNum = Console.ReadLine();
+int number = Convert.ToInt32(strNum);
+evenRange(number);
