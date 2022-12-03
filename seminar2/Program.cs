d3b42620f232466c61f153f40aa226b95b6121db
alex <alexeysadykhov@yahoo.com>
2022-12-03 15:11:49 +0400

    Task 3 done

--- seminar2/Program.cs
+++ seminar2/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Tasks 1 and 2
 int FindDigit(int n, int d)
 {
+    if (n < 0) n = n * (-1);
     List<int> remain = new List<int>();
     while (n > 0)
     {
@@ -13,7 +14,7 @@ int FindDigit(int n, int d)
 
 void Main()
 {
-    Console.WriteLine("Enter the number:");
+    Console.Write("Tasks 1-2.\nEnter the number:");
     int number = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine("Enter the digit you want to find:");
     int digit = Convert.ToInt32(Console.ReadLine());
@@ -23,3 +24,18 @@ void Main()
 }
 Main();
 
+// Task 3
+bool IsWeekday(int n)
+{
+    if (n == 6 || n == 7) return true;
+    else return false;
+}
+
+void Check()
+{
+    Console.Write("Task 3.\nEnter the number of the day:");
+    int dayNum = Convert.ToInt32(Console.ReadLine());
+    if (dayNum < 1 || dayNum > 7) Console.WriteLine($"There is no {dayNum} day in the week.");
+    else Console.WriteLine(IsWeekday(dayNum));
+}
+Check();
