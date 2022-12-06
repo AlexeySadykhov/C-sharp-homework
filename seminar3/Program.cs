4671d7f75144c39858f3fa5ac6ebc169df6f6683
alex <alexeysadykhov@yahoo.com>
2022-12-06 16:13:53 +0400

    Seminar3 folder added. First task solved

---
+++ seminar3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Task 1
+bool IsPalindrome(string x)
+{
+    string y = new string(x.Reverse().ToArray());
+    if (x == y) return true;
+    else return false;
+}
+Console.WriteLine("Enter the number:");
+string number = Console.ReadLine();
+if (!int.TryParse(number, out var num))
+{
+    Console.WriteLine($"{number} is not a number.");
+}
+else Console.WriteLine(IsPalindrome(number));
