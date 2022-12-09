26bd7f665bf5d04d10bf454d404a6f332bd4b46e
alex <alexeysadykhov@yahoo.com>
2022-12-10 00:58:22 +0400

    Some files modified

--- seminar4/Program.cs
+++ seminar4/Program.cs
@@ -26,8 +26,7 @@ int NumberToPow(int x, int n)
 }
 // Вообще есть же Math.Pow(), ну да ладно.
 
-Console.WriteLine("Task 1.");
-int num = GetNumber("Enter the number:");
+int num = GetNumber("Task 1.\nEnter the number:");
 int pow = GetNumber("Enter the pow:");
 Console.WriteLine(NumberToPow(num, pow));
 
@@ -43,36 +42,18 @@ int SumOfDigits(int x)
     return sm;
 }
 
-Console.WriteLine("Task 2.");
-Console.WriteLine(SumOfDigits(GetNumber("Enter the number:")));
+Console.WriteLine(SumOfDigits(GetNumber("Task 2.\nEnter the number:")));
 
 // Task 3
-int[] CreateArray(int size = 8, string nums = "")
+int[] CreateArray(int size = 8)
 {
-    if (nums.Length == 0)
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
     {
-        int[] array = new int[size];
-        for (int i = 0; i < size; i++)
-        {
-            array[i] = new Random().Next(0, 100);
-        }
-        return array;
+        array[i] = new Random().Next(0, 100);
     }
-    else
-    {
-        return nums.Split(" ").Select(Int32.Parse).ToArray();
-    }
+    return array;
 }
 
-Console.Write("Task 3.\nEnter the size of array or keep this field empty:");
-string arrSize = Console.ReadLine();
-Console.Write("If you already have numbers you want to convert, enter them or keep this field empty:");
-string numbers = Console.ReadLine();
-if (arrSize.Length == 0)
-{
-    Console.WriteLine(string.Join(" ", CreateArray(nums: numbers)));
-}
-else
-{
-    Console.WriteLine(string.Join(" ", CreateArray(size: Convert.ToInt32(arrSize), nums: numbers)));
-}
+Console.WriteLine("Task 3.");
+Console.WriteLine(string.Join((" "), CreateArray()));
--- seminar4/Python_homework_seminar4.py
+++ seminar4/Python_homework_seminar4.py
@@ -20,17 +20,4 @@ print(reduce(lambda a, x: a + x, map(int, input("Enter the number:"))))
 ## Ну вместо reduce можно конечно-же просто sum использовать. Это так, для красоты.
 
 ## Task 3
-
-def create_array(size = 8, nums = ""):
-    if not nums:
-        return [random.randint(0, 100) for _ in range(size)]
-    else:
-        return list(map(int, nums.split()))
-
-
-arr_size = input("Task 3.\nEnter the size of array or keep this field empty:")
-numbers = input("If you already have numbers you want to convert, enter them or keep this field empty:")
-if not arr_size:
-    print(create_array(nums=numbers))
-else:
-    print(create_array(size=int(arr_size), nums=numbers))
+print([random.randint(0, 100) for _ in range(int(input("Enter the size of array:")))])
